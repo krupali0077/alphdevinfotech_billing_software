9355c99179acddec0bd991772d886555b9ac3c22
krupali <krupali.perceptioncare@gmail.com>
2023-06-22 22:39:01 +0530

    [ADD] reports change.

--- hrms_perceptioncare/__manifest__.py
+++ hrms_perceptioncare/__manifest__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'HRMS',
+    'name': 'Utu chemical invoice create',
     'category': '',
     'sequence': 5,
-    'summary': 'HRMS',
+    'summary': 'utu billing',
     'version': '15.0.1',
     'license': 'LGPL-3',
     'price': '18.00',
     'currency': 'EUR',
-    'author': 'perception care india pvt ltd',
-    'website': 'https://perceptioncare.com/',
+    'author': 'Alphdevinfotech india pvt ltd',
+    'website': 'https://alphadevinfotech.com/',
     'description': """
         """,
-    'depends': ['account','website','web', 'mail','utm'],
+    'depends': ['account','website','web', 'mail','utm','sale_management'],
     'data': [
         'security/hrms_security_rule.xml',
         'views/all_menuitem_hide.xml',
